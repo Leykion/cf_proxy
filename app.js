a8977eafa15e6e2e4ecd388bfda5496c650d0f6f
Leykion <leykion@126.com>
2023-07-28 17:15:36 +0800

    Update app.js
    update UID

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { TextDecoder } = require('util');
 const logcb= (...args)=>console.log.bind(this,...args);
 const errcb= (...args)=>console.error.bind(this,...args);
 
-const uuid= (process.env.UUID||'d342d11e-d424-4583-b36e-524ab1f0afa4').replace(/-/g, "");
+const uuid= (process.env.UUID||'dd5dc908-4a11-480d-addd-eb2524344542').replace(/-/g, "");
 const port= process.env.PORT||3000;
 
 const wss=new WebSocket.Server({port},logcb('listen:', port));
